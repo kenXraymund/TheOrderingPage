c709368c9169d2e76c402d78fa9c09bab3468ef3
Spectre <126395355+kenXraymund@users.noreply.github.com>
2023-04-27 07:46:52 +0800

    Add files via upload

---
+++ Public/js/script.js
@@ -0,0 +1,50 @@
+ 
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+/*loader*/
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+    }
+    function fadeOut(){
+        setInterval(loader, 500);
+    }
+    window.onload = fadeOut();
+
+function log(){
+    var login = document.getElementById("login").value;
+
+    if(login == login){
+window.location.assign("loginpage.html");
+    }
+}
+//add to cart
+function Addcart(){
+
+var Add = document.getElementById("addtocart").value;
+
+if (Add == Add){
+    alert("ADDED!!");
+
+}
+}
+//payment
+function Payment(){
+
+    var payment = document.getElementById("pay").value;
+    
+    if (payment == payment){
+        alert("SUCCESS!!");
+    
+    }
+    }
